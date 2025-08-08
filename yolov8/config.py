c0bd0e4bc94abb6e8c5dfe27f0a940c7fd587d1a
Nguyen_Thanh_Hoai-DH52200700 <disbray100@gmail.com>
2025-08-09 02:36:29 +0700

    config varible

---
+++ yolov8/config.py
@@ -0,0 +1,13 @@
+from yolov8.dataset.dataset import getlistfile
+
+BATCH_SIZE = 1
+NUM_CLASSES = 1
+EPOCHS = 10
+INPUT_SHAPE = [640,640,3]
+LEARNING_RATE = 0.001
+N_MAX_Bboxes = 10
+
+
+
+images_path = getlistfile("dataset", "train")
+Total_Train = len(images_path)
